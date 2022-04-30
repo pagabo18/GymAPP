d6459b7b3baca6cc1c818737e4364b1986b59fae
diegolloz98 <is714905@iteso.mx>
2022-04-30 09:27:39 -0500

    no-jala-cart

--- lib/content/Suplementos/catalog_products.dart
+++ lib/content/Suplementos/catalog_products.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:foto_share/content/Suplementos/controllers/cart_controller.dart';
-import 'package:foto_share/content/Suplementos/models/product_model.dart';
+import 'package:foto_share/content/Suplementos/controllers/product_controller.dart';
 import 'package:get/get.dart';
+
 class CatalogProducts extends StatelessWidget {
+  final productController = Get.put(ProductController());
 
   CatalogProducts({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
+    return Obx(
+      () => Flexible(
         child: ListView.builder(
-            itemCount: Product.products.length,
+            itemCount: productController.products.length,
             itemBuilder: (BuildContext context, int index) {
               return CatalogProductCard(index: index);
             }),
-      
+      ),
     );
   }
 }
 
 class CatalogProductCard extends StatelessWidget {
   final cartController = Get.put(CartController());
+  final ProductController productController = Get.find();
   final int index;
 
   CatalogProductCard({
@@ -41,13 +45,13 @@ class CatalogProductCard extends StatelessWidget {
           CircleAvatar(
             radius: 40,
             backgroundImage: NetworkImage(
-              Product.products[index].imageUrl,
+              productController.products[index].imageUrl,
             ),
           ),
           SizedBox(width: 20),
           Expanded(
             child: Text(
-              Product.products[index].name,
+              productController.products[index].name,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
@@ -55,11 +59,11 @@ class CatalogProductCard extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Text('${Product.products[index].price}'),
+            child: Text('${productController.products[index].price}'),
           ),
           IconButton(
             onPressed: () {
-              cartController.addProduct(Product.products[index]);
+              cartController.addProduct(productController.products[index]);
             },
             icon: Icon(
               Icons.add_circle,
--- lib/content/Suplementos/models/product_model.dart
+++ lib/content/Suplementos/models/product_model.dart
@@ -11,23 +11,14 @@ class Product {
     required this.price,
     required this.imageUrl,
   });
+  static Product fromSnapshot(DocumentSnapshot snap) {
+    Product product = Product(
+      name: snap['name'],
+      price: snap['price'],
+      imageUrl: snap['imageUrl'],
+    );
+    return product;
+  }
 
-
-  static const List<Product> products = [
-    Product(
-      name: 'Nutricost Whey Protein',
-      price: 2.99,
-      imageUrl: 'https://m.media-amazon.com/images/I/71f+e1iBprL._AC_SY606_.jpg'
-    ),
-     Product(
-      name: 'Legion Whey Protein',
-      price: 9.99,
-      imageUrl: 'https://m.media-amazon.com/images/I/71ygMyeEfuL._AC_SX569_.jpg'
-    ),
-     Product(
-      name: 'Dymatize ISO 100 Protein',
-      price: 5.00,
-      imageUrl: 'https://m.media-amazon.com/images/I/616bK2U-6WL._AC_SX425_.jpg'
-    )
-  ];
+  
 }
