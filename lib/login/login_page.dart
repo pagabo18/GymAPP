40d5d3f5686b44c257e9057d5103dafd8253a223
Alvaro Gonzalez <hyruleloser@protonmail.com>
2022-05-04 13:40:46 -0500

    changed log in page design

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foto_share/auth/bloc/auth_bloc.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -13,37 +15,75 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text("GYMAPP",
-                  style: TextStyle(
-                    fontSize: 49,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.deepPurple,
-                  )),
-            ),
-            Image.asset(
-              "assets/icon/dumbbell.png",
-              height: 100,
-            ),
-            SizedBox(height: 200),
-            Text(
-              "Utiliza un red social",
-            ),
-            MaterialButton(
-              child: Text("Iniciar con Google"),
-              color: Colors.lightBlue,
-              onPressed: () {
-                BlocProvider.of<AuthBloc>(context).add(GoogleAuthEvent());
-              },
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Login'),
       ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/icon/background.webp'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, .3)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: 50),
+                    Text(
+                      "Bienvenido a GymAPP",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Image.asset(
+                      "assets/icon/dumbbell.png",
+                      height: 200,
+                    ),
+                    SizedBox(height: 150),
+                    Text(
+                      "Log in using a social media account",
+                      style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    SignInButton(
+                      Buttons.Google,
+                      onPressed: () {
+                        BlocProvider.of<AuthBloc>(context)
+                            .add(GoogleAuthEvent());
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.black54,
     );
   }
 }
