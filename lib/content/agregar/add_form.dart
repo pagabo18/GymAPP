ece25fee7ee568c20cf98ab45114810df6bde384
Alvaro Gonzalez <hyruleloser@protonmail.com>
2022-05-10 11:51:12 -0500

    added exercices to add routine

--- lib/content/agregar/add_form.dart
+++ lib/content/agregar/add_form.dart
@@ -1,9 +1,8 @@
-
-
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foto_share/content/agregar/bloc/create_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AddForm extends StatefulWidget {
   AddForm({Key? key}) : super(key: key);
@@ -17,6 +16,60 @@ class _AddFormState extends State<AddForm> {
   var _description = TextEditingController();
   var _nombre = TextEditingController();
 
+  List<dynamic> _ejerID = [
+    '1ktApH0YFtIT09dIG40z',
+    '8mjlSuqKb9PEK8jdbuuP',
+    '9vBPjRqufwvTPecXfWP4',
+    'DQdEOlbj0VreKZtZ0i8f',
+    'GYM4PYyKhvwNSiZmNrbr',
+    'GiwdKpTc8ftHcj8ZdKM8',
+    'LgfWvfVnRWPB4wYGZMyU',
+    'QPjReaF3py7H2tcXlOHs',
+    'YvxaGa5b5JPcrxvtWvyk',
+    'ZbSE05zcSP5Yes8ghqDE',
+    'p2gILzxoM3NElEkrl9r7',
+    'qhLtrbsY4dH2m5adhbXO',
+    'rYskHhxNpM8OgiQ7x6pi',
+    'tQYfgWUDBC5upvDwYMaB'
+  ];
+
+  List<dynamic> _ejercicios = [
+    'Sukhasana',
+    'Dips',
+    'Barra de press de banca',
+    'Curl de martillo',
+    'Curl con mancuernas',
+    'Dominadas',
+    'Dwi Pada Pitham',
+    'Curl con barra',
+    'Aperturas con mancuernas',
+    'Jalón con agarre neutral',
+    'Abdominales',
+    'Estiramiento de cuádriceps',
+    'Estiramiento de trampas',
+    'Elevaciones de piernas acostadas',
+  ];
+
+  //list of 14 boolean values
+  List<bool> _ejerciciosSelected = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+
+  List<dynamic> _selectedEjer = [];
+
   File? image;
   @override
   Widget build(BuildContext context) {
@@ -24,19 +77,20 @@ class _AddFormState extends State<AddForm> {
       if (state is CreatePictureErrorState) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text("Error al elegir imagen valida"),
+            content: Text("Error al elegir una imagen valida"),
           ),
         );
       } else if (state is CreateFshareErrorState) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text("Error al guardar Fshare"),
+            content: Text("Error al guardar el formulario. Intente de nuevo"),
           ),
         );
       } else if (state is CreateSuccessState) {
         _nombre.clear();
         _description.clear();
         _subtitulo.clear();
+        _ejerciciosSelected.forEach((element) => element = false);
         image = null;
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
@@ -50,6 +104,13 @@ class _AddFormState extends State<AddForm> {
       return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: ListView(children: [
+          SvgPicture.asset(
+            'assets/icon/exerVector.svg',
+            height: 200,
+            semanticsLabel: 'Exercise vector',
+          ),
+          SizedBox(height: 20),
+
           image != null
               ? Image.file(
                   image!,
@@ -57,16 +118,22 @@ class _AddFormState extends State<AddForm> {
                 )
               : Container(),
           SizedBox(height: 24),
-          MaterialButton(
-            child: Text("Foto"),
-            onPressed: () {
-              // BLoC tomar  foto
-              BlocProvider.of<CreateBloc>(context)
-                  .add(OnCreateTakePictureEvent());
-            },
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.purple,
+            child: IconButton(
+              onPressed: () {
+                // BLoC tomar  foto
+                BlocProvider.of<CreateBloc>(context)
+                    .add(OnCreateTakePictureEvent());
+              },
+              color: Colors.white,
+              icon: Icon(Icons.image),
+            ),
           ),
           SizedBox(height: 24),
           TextField(
+            maxLength: 30,
             controller: _nombre,
             decoration: InputDecoration(
               label: Text("Nombre"),
@@ -75,34 +142,86 @@ class _AddFormState extends State<AddForm> {
           ),
           SizedBox(height: 24),
           TextField(
+            maxLines: null,
             controller: _description,
             decoration: InputDecoration(
-              label: Text("descripcion"),
+              label: Text("Descripcion"),
               border: OutlineInputBorder(),
             ),
           ),
           SizedBox(height: 24),
           TextField(
+            maxLength: 60,
+            maxLines: null,
             controller: _subtitulo,
             decoration: InputDecoration(
-              label: Text("subtitulo"),
+              label: Text("Subtitulo"),
               border: OutlineInputBorder(),
             ),
           ),
           SizedBox(height: 24),
+          //divider
+          Text(
+            "Ejercicios",
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          Divider(
+            color: Colors.black54,
+          ),
+
+          Column(
+            children: _ejercicios.map((ejer) {
+              int index = _ejercicios.indexOf(ejer);
+              return CheckboxListTile(
+                title: Text(ejer),
+                value: _ejerciciosSelected[index],
+                onChanged: (value) {
+                  setState(() {
+                    _ejerciciosSelected[index] = value!;
+                  });
+                },
+              );
+            }).toList(),
+          ),
+
           MaterialButton(
             child: Text("Guardar"),
             onPressed: () {
-              Map<String, dynamic> fshareData = {};
-              fshareData = {
-                "nombre": _nombre.value.text,
-                "descripcion": _description.value.text,
-                "subtitulo": _subtitulo.value.text,
-                "ejercicios": ["1ktApH0YFtIT09dIG40z", "LgfWvfVnRWPB4wYGZMyU"],
-              };
-              BlocProvider.of<CreateBloc>(context)
-                  .add(OnCreateSaveDataEvent(dataToSave: fshareData));
+              if (_nombre.text.isEmpty ||
+                  _description.text.isEmpty ||
+                  _subtitulo.text.isEmpty) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Por favor llene todos los campos"),
+                  ),
+                );
+              } else {
+                _selectedEjer = [];
+                var index = 0;
+                for (var bool in _ejerciciosSelected) {
+                  if (bool) {
+                    _selectedEjer.add(_ejerID[index]);
+                  }
+                  index++;
+                }
+                print("Selected ejer: $_selectedEjer");
+
+                Map<String, dynamic> fshareData = {};
+                fshareData = {
+                  "nombre": _nombre.value.text,
+                  "descripcion": _description.value.text,
+                  "subtitulo": _subtitulo.value.text,
+                  "ejercicios": _selectedEjer,
+                };
+
+                print("fshareData: $fshareData");
+
+                BlocProvider.of<CreateBloc>(context)
+                    .add(OnCreateSaveDataEvent(dataToSave: fshareData));
+              }
             },
+            color: Colors.blue,
           ),
         ]),
       );
